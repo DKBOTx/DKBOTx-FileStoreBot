0304f6fa3d8a819493f2983b0843421c1e95a962
dk [OFLINE] <98028377+vloggerdeven@users.noreply.github.com>
2022-03-04 00:04:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/vloggerdeven/File-Sharing-Bot
+  git clone https://github.com/vloggerdeven/File-share-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /File-sharing-Bot
+  git clone $UPSTREAM_REPO /File-share-bot
 fi
-cd /File-Sharing-Bot
+cd /File-share-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
